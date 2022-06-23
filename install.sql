43d625251e8610a7b086cab5a2faed212be0998a
Marco Daries <deraiser@gmx.de>
2022-06-23 06:14:03 +0200

    add faction db structur

--- install.sql
+++ install.sql
@@ -1,3 +1,14 @@
+DROP TABLE IF EXISTS rp1_faction;
+CREATE TABLE rp1_faction (
+    factionID INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    packageID INT(10) NOT NULL,
+    gameID INT(10) NOT NULL,
+    identifier VARCHAR(191) NOT NULL,
+    icon VARCHAR(255) NOT NULL DEFAULT '',
+    isDisabled TINYINT(1) NOT NULL DEFAULT 0,
+    UNIQUE KEY identifier (identifier, gameID)
+);
+
 DROP TABLE IF EXISTS rp1_game;
 CREATE TABLE rp1_game (
     gameID INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -12,4 +23,7 @@ CREATE TABLE rp1_game (
 /* SQL_PARSER_OFFSET */
 
 -- foreign keys
+ALTER TABLE rp1_faction ADD FOREIGN KEY (gameID) REFERENCES rp1_game (gameID) ON DELETE CASCADE;
+ALTER TABLE rp1_faction ADD FOREIGN KEY (packageID) REFERENCES wcf1_package (packageID) ON DELETE CASCADE;
+
 ALTER TABLE rp1_game ADD FOREIGN KEY (packageID) REFERENCES wcf1_package (packageID) ON DELETE CASCADE;
