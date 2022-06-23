37369a5d8c32d65f94c6cae3d2186a01d1c65345
Marco Daries <deraiser@gmx.de>
2022-06-23 06:15:16 +0200

    add race db structur

--- install.sql
+++ install.sql
@@ -20,6 +20,24 @@ CREATE TABLE rp1_game (
     UNIQUE KEY identifier (identifier)
 );
 
+DROP TABLE IF EXISTS rp1_race;
+CREATE TABLE rp1_race (
+    raceID INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    packageID  INT(10) NOT NULL,
+    gameID INT(10) NOT NULL,
+    identifier VARCHAR(191) NOT NULL,
+    icon VARCHAR(255) NOT NULL DEFAULT '',
+    isDisabled TINYINT(1) NOT NULL DEFAULT 0,
+    UNIQUE KEY identifier (identifier, gameID)
+);
+
+DROP TABLE IF EXISTS rp1_race_to_faction;
+CREATE TABLE rp1_race_to_faction (
+    raceID INT(10) NOT NULL,
+    factionID INT(10) NOT NULL,
+    UNIQUE KEY(raceID, factionID)
+);
+
 /* SQL_PARSER_OFFSET */
 
 -- foreign keys
@@ -27,3 +45,8 @@ ALTER TABLE rp1_faction ADD FOREIGN KEY (gameID) REFERENCES rp1_game (gameID) ON
 ALTER TABLE rp1_faction ADD FOREIGN KEY (packageID) REFERENCES wcf1_package (packageID) ON DELETE CASCADE;
 
 ALTER TABLE rp1_game ADD FOREIGN KEY (packageID) REFERENCES wcf1_package (packageID) ON DELETE CASCADE;
+
+ALTER TABLE rp1_race ADD FOREIGN KEY (gameID) REFERENCES rp1_game (gameID) ON DELETE CASCADE;
+ALTER TABLE rp1_race ADD FOREIGN KEY (packageID) REFERENCES wcf1_package (packageID) ON DELETE CASCADE;
+ALTER TABLE rp1_race_to_faction ADD FOREIGN KEY (raceID) REFERENCES rp1_race (raceID) ON DELETE CASCADE;
+ALTER TABLE rp1_race_to_faction ADD FOREIGN KEY (factionID) REFERENCES rp1_faction (factionID) ON DELETE CASCADE;
